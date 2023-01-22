4aa0fa1a7b9bd0a347bebdf5ccbc311d9002f05f
Richard Chalk <barmybrit@gmail.com>
2023-01-22 10:15:30 +0100

    Final

--- FACIT/index.js
+++ FACIT/index.js
@@ -20,7 +20,7 @@ buttonTwo.addEventListener("click", alertButton);
 // 3.
 // a. Hämta hela box 3 till en variabel.
 // b. Skapa en ny css class som gör bakgrundsfärgen grön
-// c. Skapa en callback function som toggle din nya class
+// c. Skapa en callback function som 'toggle' din nya class
 // d. Lägg till din "mouseover" eventlistener som anropar din callback metod
 const boxThree = document.querySelector(".box-3");
 
@@ -29,3 +29,53 @@ function newBgColour() {
 }
 
 boxThree.addEventListener("mouseover", newBgColour);
+
+// Button 4
+// 4.
+// a. Hämta knappen till en variabel
+// b. Vid mouse click ändra färgen på bakgrunden inom knappen
+// c. Skapa 2 nya klasser... med en färg bakgrund... och en med ingen färg bakgrund
+// d. Använd en if sats för att byta mellan (vid klick) färg och ingen färg
+
+const btnFour = document.querySelector(".btn-4");
+
+function newColour() {
+  if (btnFour.classList.contains("redBg")) {
+    btnFour.classList.add("noBg");
+    btnFour.classList.remove("redBg");
+  } else {
+    btnFour.classList.add("redBg");
+    btnFour.classList.remove("noBg");
+  }
+}
+
+btnFour.addEventListener("click", newColour);
+
+// Button 5
+// 5.
+// a. Hämta knappen till en variabel
+// b. Skapa en metod som ändrar rubriktexten i box 5 till italic
+
+const btnFive = document.querySelector(".btn-5");
+const headingFive = document.querySelector("#heading-5");
+
+function italic() {
+  headingFive.style.fontStyle = "italic";
+}
+
+btnFive.addEventListener("click", italic);
+
+// Button 6
+// 6.
+// a. Hämta knappen till en variabel
+// b. Skapa en metod som ändrar bakgrundsfärgen på hela sidan
+
+const btnSix = document.querySelector(".btn-6");
+
+function bgBody() {
+  document.body.style.background = "green";
+}
+
+btnSix.addEventListener("click", bgBody);
+
+console.log(btnSix);
--- index.js
+++ index.js
@@ -15,3 +15,20 @@
 // b. Skapa en ny css class som gör bakgrundsfärgen grön
 // c. Skapa en callback function som toggle din nya class
 // d. Lägg till din "mouseover" eventlistener som anropar din callback metod
+
+// Button 4
+// 4.
+// a. Hämta knappen till en variabel
+// b. Vid mouse click ändra färgen på bakgrunden inom knappen
+// c. Skapa 2 nya klasser... med en färg bakgrund... och en med ingen färg bakgrund
+// d. Använd en if sats för att byta mellan (vid klick) färg och ingen färg
+
+// Button 5
+// 5.
+// a. Hämta knappen till en variabel
+// b. Skapa en metod som ändrar rubriktexten i box 5 till italic
+
+// Button 6
+// 6.
+// a. Hämta knappen till en variabel
+// b. Skapa en metod som ändrar bakgrundsfärgen på hela sidan
